8f6fea56d4be654ab1e1b89bb29e704543bbb0e8
filipivko <tesikf@seznam.cz>
2024-06-12 23:07:50 +0200

    Task 4 done (why in chosen has to be int(input and why only input doesnt work?)

--- Homework rep2.py
+++ Homework rep2.py
@@ -1,3 +1,26 @@
 #task4
 '''Write a function that removes a given number from the list of integers.
 Return the number of removed elements from the function.'''
+
+
+def remove(numbers, target, removed_numbers):
+    if target in numbers:
+        numbers.remove(target)
+        removed_numbers.append(target)
+    else:
+        print(f"{target} not found")
+    return
+
+def main():
+    list = [1, 2, 3, 4, 5]
+    removed = []
+    while True:
+        print (f"list of numbers: {list}")
+        print (f"Removed numbers: {removed}")
+        choice = int(input("which number would you like to remove?:"))
+        remove(list, choice, removed)
+
+list = [1, 2, 3, 4, 5]
+removed = []
+if __name__ == "__main__":
+    main()
