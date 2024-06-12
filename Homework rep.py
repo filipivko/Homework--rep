166fd3709a89453b4cfca367a3dc4942b38bb0aa
filipivko <tesikf@seznam.cz>
2024-06-12 22:06:16 +0200

    Task 3

--- Homework rep.py
+++ Homework rep.py
@@ -1,6 +1,8 @@
 #Task1
 '''Write a function that calculates the product of the elements from a list of integers.
 The list is passed as a parameter. The result is returned from the function.'''
+import math
+
 
 def amount(numbers):
     return len(numbers)
@@ -12,7 +14,7 @@ def task1(numbers):
     return product
 
 list1 = [1, 2, 3, 4]
-list2 = [15, 64, 86, 88, 10, 68, 456]
+list2 = [15, 64, 86, 88, 10, 68, 456, 11]
 print (f"number of elemetns in list1 is {amount(list1)}")
 print (f"number of elemetns in list2 is {amount(list2)}")
 print (f"product of list1 is {task1(list1)}")
@@ -29,3 +31,30 @@ def task2(numbers):
 
 print (f"smallest number from list1 is {task2(list1)}")
 print (f"smallest number from list2 is {task2(list2)}")
+
+#Task3
+'''Write a function that determines how many prime numbers there are in the list of integers. 
+The list is passed as a parameter. The result is returned from the function.'''
+
+def is_prime(n):
+    if n <=1:
+        return False
+    if n <=3:
+        return True
+    if n > 2 and n % 2 ==0:
+        return False
+
+    max_divisor = math.floor(math.sqrt(n))
+    for d in range(3, 1 + max_divisor, 2):
+        if n % d == 0:
+            return False
+    return True
+
+def count_primes(numbers):
+    count = 0
+    for number in numbers:
+        if is_prime(number):
+            count += 1
+    return count
+print (f"amount of primes in list1 is {count_primes(list1)}")
+print (f"amount of primes in list2 is {count_primes(list2)}")
---
+++ Homework rep2.py
@@ -0,0 +1,3 @@
+#task4
+'''Write a function that removes a given number from the list of integers.
+Return the number of removed elements from the function.'''
